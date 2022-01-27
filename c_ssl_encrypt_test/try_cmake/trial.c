b45e5733f77a5669d00a99f4f1a5aaa438ea2222
TroyC@MSI <troychan279@gmail.com>
2022-01-27 17:57:37 +0800

    try_cmake

---
+++ c_ssl_encrypt_test/try_cmake/trial.c
@@ -0,0 +1,69 @@
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <string.h>
+#include "dencrypt.h"
+
+int main (int argc, unsigned char **argv[])
+{
+    /*
+     * Set up the key and iv. Do I need to say to not hard code these in a
+     * real application? :-)
+     */
+
+    /* A 256 bit key */
+    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
+    /* A 128 bit IV */
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+
+    /* Message to be encrypted */
+    unsigned char *plaintext;
+    if (argc > 1) {
+        plaintext = (unsigned char *)argv[1];
+    } else {
+        plaintext = 
+            (unsigned char *)"Message\n to be encaaaaaaaaaaaaaaaaaaaarypted~~~\n~~~";
+    }
+    printf("Input text: %s\n", plaintext);
+    /*
+     * Buffer for ciphertext. Ensure the buffer is long enough for the
+     * ciphertext which may be longer than the plaintext, depending on the
+     * algorithm and mode.
+     */
+    unsigned char ciphertext[128];
+
+    /* Buffer for the decrypted text */
+    unsigned char decryptedtext[128];
+
+    int decryptedtext_len, ciphertext_len;
+
+    /* Encrypt the plaintext */
+    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+                              ciphertext);
+
+    /* Do something useful with the ciphertext here */
+    printf("Ciphertext is:\n");
+    for (int i = 0; i < ciphertext_len;i++)
+    {
+    	printf("%02x", ciphertext[i]);
+    }
+    printf("\n");
+    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+
+    printf("Ciphertext Length:%ld\n", strlen(ciphertext));
+
+    /* Decrypt the ciphertext */
+    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+                                decryptedtext);
+
+    /* Add a NULL terminator. We are expecting printable text */
+    decryptedtext[decryptedtext_len] = '\0';
+
+    /* Show the decrypted text */
+    printf("Decrypted text is:\n");
+    printf("%s\n", decryptedtext);
+
+
+    return 0;
+}
